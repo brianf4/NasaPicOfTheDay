78ff9f285b6bd22d9dbd564d98624a1dd806fea2
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-04-26 00:20:24 -0700

    minor css changes

--- js/main.js
+++ js/main.js
@@ -20,3 +20,17 @@ fetch(url)
 });
 }
 
+
+
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=bgzOkibs3a1OAkn1d0hcXllgWdz8eF7CS7pycdew`)
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+ // console.log(data)
+  document.querySelector('img').src = data.hdurl
+  document.querySelector('h1').innerText = data.title
+  document.querySelector('p').innerText = data.explanation
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
