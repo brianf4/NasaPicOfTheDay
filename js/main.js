0d32137be5298adffbc0620ee683c174481d3712
Brian Fuentes <brian.fuentes04@gmail.com>
2022-04-25 18:23:54 -0700

    updated files with api requests

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-const url = `https://api.nasa.gov/planetary/apod?api_key=bgzOkibs3a1OAkn1d0hcXllgWdz8eF7CS7pycdew`
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+const choice = document.querySelector('input').value
+console.log(choice)
+
+const url = `https://api.nasa.gov/planetary/apod?api_key=bgzOkibs3a1OAkn1d0hcXllgWdz8eF7CS7pycdew&date=${choice}`
 
 
 fetch(url)
 .then(res => res.json()) // parse response as JSON
 .then(data => {
-  console.log(data)
+ // console.log(data)
   document.querySelector('img').src = data.hdurl
   document.querySelector('h1').innerText = data.title
   document.querySelector('p').innerText = data.explanation
@@ -12,5 +18,5 @@ fetch(url)
 .catch(err => {
     console.log(`error ${err}`)
 });
-
+}
 
